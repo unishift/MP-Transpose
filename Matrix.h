0dec5cb77d41da044503997884d609203cb64af7
unishift <unishifft@gmail.com>
2018-11-09 18:19:11 +0300

    Add getters for rows and cols of Matrix

--- Matrix.h
+++ Matrix.h
@@ -1,27 +1,38 @@
-#include <iostream>
-#include <omp.h>
-
 template <typename T>
 class Matrix {
     T* ptr;
     size_t _rows;
     size_t _cols;
 public:
-    Matrix(size_t rows, size_t cols) :
-        ptr(new T[rows * cols]),
-        _rows(rows), _cols(cols)
-    { }
-
-    ~Matrix() {
-        delete [] ptr;
-    }
-
     T& operator()(size_t i, size_t j) {
         return ptr[i * _cols + j];
     }
 
     const T& operator()(size_t i, size_t j) const {
         return ptr[i * _cols + j];
+    }
+
+    Matrix(size_t rows, size_t cols) :
+        ptr(new T[rows * cols]),
+        _rows(rows), _cols(cols) {
+    
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                (*this)(i, j) = i - j;
+            }
+        }
+    }
+
+    ~Matrix() {
+        delete [] ptr;
+    }
+
+    size_t rows() const {
+        return _rows;
+    }
+
+    size_t cols() const {
+        return _cols;
     }
 
 };
