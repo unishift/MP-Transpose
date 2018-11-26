2bbba53647a23023f88e48a946fc0583a36bcbde
unishift <unishifft@gmail.com>
2018-11-26 14:18:37 +0300

    Rework run_tests.sh to work only with square matrices

--- run_tests.sh
+++ run_tests.sh
@@ -1,35 +1,14 @@
 #!/bin/bash
 
 command=$1
-max_threads=$2
-output_file=$3
+side=$2
+max_threads=$3
+output_file=$4
 
-for (( i = 0; i < 3; i++ ))
+for (( i = 0; i < 5; i++ ))
 do
-    # rows < cols
-    for (( rows = 1000, cols = 3000; cols < 70000; rows += 4000, cols += 12000 ))
+    for (( num_threads = 1; num_threads <= max_threads; num_threads *= 2 ))
     do
-        for (( num_threads = 1; num_threads <= max_threads; num_threads *= 2 ))
-        do
-            bsub -n ${num_threads} -W 1 -e ${output_file} ${command} ${rows} ${cols} ${num_threads}
-        done
-    done
-
-    # rows > cols
-    for (( rows = 3000, cols = 1000; rows < 70000; rows += 12000, cols += 4000 ))
-    do
-        for (( num_threads = 1; num_threads <= max_threads; num_threads *= 2 ))
-        do
-            bsub -n ${num_threads} -W 1 -e ${output_file} ${command} ${rows} ${cols} ${num_threads}
-        done
-    done
-
-    # rows == cols
-    for (( rows = 3000, cols = 3000; cols < 70000; rows += 12000, cols += 12000 ))
-    do
-        for (( num_threads = 1; num_threads <= max_threads; num_threads *= 2 ))
-        do
-            bsub -n ${num_threads} -W 1 -e ${output_file} ${command} ${rows} ${cols} ${num_threads}
-        done
+        bsub -n ${num_threads} -W 1 -e ${output_file} ${command} ${side} ${side} ${num_threads}
     done
 done
